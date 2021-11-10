a818d57222bca98ec14ff05930f3c43fedce0116
RoseGabriely <rosegabriely14@gmail.com>
2021-11-10 01:09:55 -0500

    completed auth middlewares

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const Users = require("../users/users-model");
+
 function restricted(req, res, next) {
   if (req.session) {
     next();
@@ -6,38 +8,41 @@ function restricted(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body already exists in the database
+function checkUsernameFree(req, res, next) {
+  Users.findBy({ username: req.body.username })
+    .then((user) => {
+      if (!user.length) {
+        next();
+      } else {
+        next({ status: 422, message: "Username taken" });
+      }
+    })
+    .catch(next);
+}
 
-  status 422
-  {
-    "message": "Username taken"
+function checkUsernameExists(req, res, next) {
+  Users.findBy({ username: req.body.username })
+    .then((user) => {
+      if (!user.length) {
+        next();
+      } else {
+        next({ status: 401, message: "Invalid credentials" });
+      }
+    })
+    .catch(next);
+}
+
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password < 3) {
+    next({ status: 422, message: "Password must be longer than 3 chars" });
+  } else {
+    next();
   }
-*/
-function checkUsernameFree() {}
-
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
-function checkUsernameExists() {}
-
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
-function checkPasswordLength() {}
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
+}
 
 module.exports = {
   restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
 };
